265764bb0e893e3efde91fc7d1092b43f95aa3a7
Gustavo Harff <gustavo.harff@gmail.com>
2022-05-13 20:10:23 -0300

    Initial Commit

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Gustavo Harff on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
